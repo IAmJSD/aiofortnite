3d5f998d78344d72b4881e098c3a41c6c106528f
Jake Gealer <jake@gealer.email>
2018-07-23 16:20:55 +0100

    Changed version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiofortnite",
-    version="0.43",
+    version="0.53",
     description="A asyncio wrapper for Fortnite.",
     author="Jake Gealer",
     author_email="jake@gealer.email",
